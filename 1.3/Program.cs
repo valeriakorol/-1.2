00eb0c937cbbb07e13bbd1c6dcacade44204d2e1
korol <korol@VALERIE>
2024-03-06 00:05:48 +0200

    Добавьте файлы проекта.

---
+++ 1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Введіть значення D: ");
+        int D = int.Parse(Console.ReadLine()); 
+        List<int> A = new List<int> { 15, 23, 36, 47, 50, 62, 74, 85, 98, 6, 8, 21, };
+
+        var result = A.Where(num => num > 0 && num % 10 == D) 
+                      .Reverse() 
+                      .Distinct() 
+                      .Reverse(); 
+
+        Console.WriteLine("Результат:");
+        foreach (var num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ прога 1.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>
+        {
+            { "01", "січень" },
+            { "02", "лютий" },
+            { "03", "березень" },
+            { "04", "квітень" },
+            { "05", "травень" },
+            { "06", "червень" },
+            { "07", "липень" },
+            { "08", "серпень" },
+            { "09", "вересень" },
+            { "10", "жовтень"},
+            { "11", "листопад" },
+            { "12", "грудень" }
+        };
+
+        Console.Write("Введіть значення ключа: ");
+        string key = Console.ReadLine();
+
+        Dictionary<string, string> filteredDictionary = new Dictionary<string, string>();
+
+        foreach (KeyValuePair<string, string> entry in dictionary)
+        {
+            if (string.Compare(entry.Key, key) >= 0)
+            {
+                filteredDictionary.Add(entry.Key, entry.Value);
+            }
+        }
+
+        if (filteredDictionary.Count > 0)
+        {
+            string json = JsonConvert.SerializeObject(filteredDictionary);
+            Console.WriteLine("Результат:");
+            Console.WriteLine(json);
+            File.WriteAllText("result.json", json);
+        }
+        else
+        {
+            Console.WriteLine("null");
+        }
+    }
+}
